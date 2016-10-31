578591f0df83f7087a7bbeeac36ad1d33b88cd6c
raboy1997 <maks.rublya@mail.ru>
2016-10-31 12:14:26 +0200

    deploy element

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -83,4 +83,10 @@ Rails.application.configure do
 
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
+
+  config.web_socket_server_url = "wss://chat-applikeysolutions.herokuapp.com/cable"
+
+  config.action_cable.allowed_request_origins = ['https://chat-applikeysolutions.herokuapp.com', 'http://chat-applikeysolutions.herokuapp.com']
+
+
 end
